6762b33cd5cd24d07d60156c4f0e7a98c5be612b
bhefny playbot18 <bhefny@gmail.com>
2018-06-20 09:43:45 +0200

    squash me later

--- pre_compose.sh
+++ pre_compose.sh
@@ -7,11 +7,15 @@ source .env_vars
 cat .env_vars > .env
 echo "GID=$GID" >> .env
 echo "UID=$UID" >> .env
-sort -u .env > .env
 
 mkdir -p $MYPLEX_MOVIE
 mkdir -p $MYPLEX_TV
 mkdir -p $MYPLEX_CONFIG
 mkdir -p $JD2_CONFIG
+
+if [ -z "$JD2_password" ]; then
+    echo "----------- Failed to find MyJDownloader ENV"
+    exit 1
+fi
 
 echo "{'password':'$JD2_password', 'email':'$JD2_email'}" > "$JD2_CONFIG/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
