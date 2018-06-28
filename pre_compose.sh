c5129129d1a3cc05efe9f918224be4c8629fc0f4
bhefny-dievision <hefny@dievision.de>
2018-06-28 12:36:12 +0200

    adding more folders

--- pre_compose.sh
+++ pre_compose.sh
@@ -3,11 +3,7 @@
 # https://ubuntuforums.org/showthread.php?t=2391646
 
 MY_FOLDER=/mycontent
-MY_PATH=/home/bhefny/$MY_FOLDER
-JD2_CONFIG=$MY_PATH/jd2
-MYPLEX_MOVIE=$MY_PATH/plex/movie
-MYPLEX_TV=$MY_PATH/plex/tv
-MYPLEX_CONFIG=$MY_PATH/plex/config
+MY_PATH=/home/bhefny$MY_FOLDER
 
 if [[ $(cat /etc/mtab | grep $MY_FOLDER) ]]; then
     echo "-------- encfs 👍👍"
@@ -15,25 +11,32 @@ else
     # encfs -o allow_root /home/bhefny/.encrypted /home/bhefny/mycontent
     # fusermount -u /home/bhefny/mycontent
     echo "----------- Failed to find encfs folder"
-    exit 1
+    # exit 1
 fi
 
-# create .env file to be used in docker compose
-echo "JD2_CONFIG=$JD2_CONFIG" > .env
-echo "MYPLEX_MOVIE=$MYPLEX_MOVIE" >> .env
-echo "MYPLEX_TV=$MYPLEX_TV" >> .env
-echo "MYPLEX_CONFIG=$MYPLEX_CONFIG" >> .env
+# create file then append
+echo "UID=$UID" > .env
 echo "GID=$GID" >> .env
-echo "UID=$UID" >> .env
 
-mkdir -p $MYPLEX_MOVIE
-mkdir -p $MYPLEX_TV
-mkdir -p $MYPLEX_CONFIG
-mkdir -p $JD2_CONFIG
+declare -A all_path=(
+  ["JD2_CONFIG"]=$MY_PATH/jd2
+  ["MYPLEX_CONFIG"]=$MY_PATH/plex/config
+  ["MYPLEX_TV"]=$MY_PATH/plex/tv
+  ["MYPLEX_MOVIE"]=$MY_PATH/plex/movie
+  ["MYPLEX_ANIME"]=$MY_PATH/plex/anime
+  ["MYPLEX_CARTOON_AR"]=$MY_PATH/plex/cartoon_ar
+  )
+for i in "${!all_path[@]}"
+do
+  # echo "$i=>${all_path[$i]}"
+  mkdir -p ${all_path[$i]}
+  echo "$i=${all_path[$i]}" >> .env
+done
+
 
 if [ -z "$JD2_password" ]; then
     echo "----------- Failed to find MyJDownloader ENV"
     exit 1
 fi
 
-echo "{\"password\":\"$JD2_password\", \"email\":\"$JD2_email\"}" > "$JD2_CONFIG/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
+echo "{\"password\":\"$JD2_password\", \"email\":\"$JD2_email\"}" > "${all_path['JD2_CONFIG']}/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
